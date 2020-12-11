b88c309e05abf9d9106dd466b1bd3649297abb7d
sarasandager <sara590x@edu.easj.dk>
2020-12-11 11:58:18 +0100

    Risk Manager code added

---
+++ src/sample/RiskManagerController.java
@@ -0,0 +1,12 @@
+/*
+ *
+ * Developed by Sara Sandager (sara590x@edu.easj.dk)
+ * Licensed under the MIT License
+ * 11/12/2020
+ *
+ */
+
+package sample;
+
+public class RiskManagerController {
+}
